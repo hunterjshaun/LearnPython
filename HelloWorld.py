c05c96267ef3a38bc384adf99ccd4f18f0634784
Josh <josh@mail.com>
2022-11-29 21:46:57 +0000

    Edited HellowWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,15 @@
 print("hello world")
 
-# test tokens
+#creating an empty list
+Services = []
+
+print(Services)
+
+#appending a list
+#1
+Services.append('EC2')
+print(Services)
+
+#2
+services = Services + ['Lambda', 'IAM']
+print(services)
