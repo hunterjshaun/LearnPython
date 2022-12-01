c3f69b3836a831b2ab9e613dd3587aa94a4fa728
Josh <josh@mail.com>
2022-12-01 23:27:40 +0000

    list

--- Lists.py
+++ Lists.py
@@ -11,3 +11,15 @@ print(mylist[3])
 # Length of a list
 print(len(mylist))
 
+# functions and methods of list
+mylist.append(9)
+mylist.insert(0, 0)
+print(mylist)
+mylist.append(6)
+print(sorted(mylist))
+
+# Nested lists 
+my_matrix = [[1,2,3], [4,5,6]]
+
+
+
