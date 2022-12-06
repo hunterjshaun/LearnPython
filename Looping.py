16c3007fa88f16267532dd32f86a9bce782108ab
Josh <josh@mail.com>
2022-12-06 16:44:10 +0000

    added looping.py

---
+++ Looping.py
@@ -0,0 +1,26 @@
+#While loop
+#while 1 < 2:
+    #print('something')
+
+count = 1
+while count <= 4:
+    print("looping")
+    count += 1
+    
+#FOR LOOP
+#Iterating a list
+colors = ['blue','purple','green']
+for color in colors:
+    print('This color is ' + color)
+
+#Iterating a tuple
+#Works similar to a list
+point = (1,2,3)
+for pnt in point:
+    print(pnt)
+
+
+
+
+    
+
