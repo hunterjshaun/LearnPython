cd69c676709f34ef5eb6c2d8ee84d2b5f22940c4
Josh <josh@mail.com>
2022-12-07 05:39:11 +0000

    Created Looping.py

--- Looping.py
+++ Looping.py
@@ -6,6 +6,9 @@ count = 1
 while count <= 4:
     print("looping")
     count += 1
+  
+  
+  
     
 #FOR LOOP
 #Iterating a list
@@ -18,6 +21,53 @@ for color in colors:
 point = (1,2,3)
 for pnt in point:
     print(pnt)
+
+
+
+#Iterating dictionaries
+ages = {'kevin' : 59, 'bob' : 40, 'kayla' : 21}
+for key in ages:
+    print(key)
+    
+for key, value in ages.items():
+    print(key,value)
+
+#NESTING LOOPS AND CONDITIONS
+counter = 1
+while counter <= 25:
+    if counter % 4 == 0:
+        print(counter)
+    counter += 1
+
+
+
+
+#Controlling loop execution
+count = 0
+while count < 10:
+    if count % 2 == 0:
+        count += 1
+        continue
+    print(f"We are counting odd numbers: {count}")
+    count += 1
+
+
+count = 1
+while count < 10:
+    if count % 2 == 0:
+        break
+    print(f"We are counting odd nubmer: {count}")
+    count += 1
+
+
+colors = ['blue', 'green', 'red', 'purple']
+for color in colors:
+    if color == 'blue':
+        continue
+    elif color == 'red':
+        break
+    print(color)
+
 
 
 
