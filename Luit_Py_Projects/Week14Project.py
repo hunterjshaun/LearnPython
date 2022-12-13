08c3799a5e31f5c5b18dea329af7f5fe76e3444e
Josh <josh@mail.com>
2022-12-13 17:53:07 +0000

    updated client varible

--- Luit_Py_Projects/Week14Project.py
+++ Luit_Py_Projects/Week14Project.py
@@ -2,13 +2,14 @@ import boto3
 
 
 region = 'us-east-1'
-ec2 = boto3.client('ec2',region_name=region)
+ec2_client = boto3.client('ec2',region_name=region)
 
-dev_instances = ec2.describe_tags(Filters=[{'Name': 'tag:Enviroment', 'Values': ['Dev']}])
-running_instances = ec2.describe_instance_status()
-for instances in dev_instances:
-    if instances in running_instances:
-        id='i-00b1994deb4a25354'
-        ec2.start_instances(InstanceIds=[id])
+dev_instances = ec2_client.describe_tags(Filters=[{'Name': 'tag:Enviroment', 'Values': ['Dev']}])
+running_instances = ec2_client.describe_instance_status()
+#for instances in dev_instances:
+    #if instances in running_instances and dev_instances:
+        #id=
+        #ec2_client.start_instances(InstanceIds=[id])
 #$response = client.start_instances(InstanceIds=[id],)
 #print(response)
+print(dev_instances)
