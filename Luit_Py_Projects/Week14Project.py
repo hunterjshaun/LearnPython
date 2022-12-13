8495f337a5e32bcc6ad6e2befc9c995eb5b86f75
Josh <josh@mail.com>
2022-12-13 17:46:35 +0000

    edited client variable

--- Luit_Py_Projects/Week14Project.py
+++ Luit_Py_Projects/Week14Project.py
@@ -2,12 +2,13 @@ import boto3
 
 
 region = 'us-east-1'
-client = boto3.client('ec2',region_name=region)
+ec2 = boto3.client('ec2',region_name=region)
 
-dev_instances = client.describe_tags(Filters=[{'Name': 'tag:Enviroment', 'Values': ['Dev']}])
+dev_instances = ec2.describe_tags(Filters=[{'Name': 'tag:Enviroment', 'Values': ['Dev']}])
+running_instances = ec2.describe_instance_status()
 for instances in dev_instances:
-    response = client.describe_instances(Filters=InstanceIds=[])
-    #id=ResourceId
-    #client.start_instances(InstanceIds=[id])
-#$response = client.start_instances(InstanceIds=['i-00b1994deb4a25354'],)
-print(response)
+    if instances in running_instances:
+        id='i-00b1994deb4a25354'
+        ec2.start_instances(InstanceIds=[id])
+#$response = client.start_instances(InstanceIds=[id],)
+#print(response)
