8897ecae0d18aad70819ce7826fbe469b4974c4f
Josh <josh@mail.com>
2022-12-13 23:36:20 +0000

    modified week14project.py

--- Luit_Py_Projects/week14project2.py
+++ Luit_Py_Projects/week14project2.py
@@ -7,12 +7,29 @@ ec2 = boto3.resource('ec2', region_name=region)
 
 dev_instances = ec2_client.describe_tags(Filters=[{'Name': 'tag:Enviroment', 'Values': ['Dev']}])
 running_instances = ec2_client.describe_instance_status()
-for instances in dev_instances:
-    if instances in running_instances and dev_instances:
-        id=dev_instances['Tags'][0]['ResourceId']
-        ec2_client.start_instances(InstanceIds=[id])
+dev_instance_tags = dev_instances['Tags']
+dev_instance_Id = []
+for i in dev_instance_tags:
+    dev_instance_Id.append(i['ResourceId'])
+
+for i in dev_instances:
+    if i in running_instances and dev_instances:
+        for ids in dev_instance_Id:
+            id=dev_instance_Id
+            ec2_client.stop_instances(InstanceIds=[id])
+    
+#for instances in dev_instances:
+    #if instances in running_instances and dev_instances:
+        #id=dev_instances['Tags'][0]['ResourceId']
+        #ec2_client.start_instances(InstanceIds=[id])
 #$response = client.start_instances(InstanceIds=[id],)
-#print(dev_instances)
-#print(dev_instances.get['ResourceId'])
-#dev = dev_instances['Tags'][0]['ResourceId']
+
+#print(dev_inst.get['ResourceId'])
+#dev = dev_instances['Tags']
+#dev_instId = []
+#for value in dev:
+   #dev_instId.append(value['ResourceId'])
 #print(dev)
+#print(dev)
+#print(dev_inst_tag)
+#print(dev2)
