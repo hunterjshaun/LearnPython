2f96f38947c99093040cfeb121db52154a117852
Josh <josh@mail.com>
2022-12-13 18:38:25 +0000

    created week 14 project 2

--- Luit_Py_Projects/week14project2.py
+++ Luit_Py_Projects/week14project2.py
@@ -7,10 +7,12 @@ ec2 = boto3.resource('ec2', region_name=region)
 
 dev_instances = ec2_client.describe_tags(Filters=[{'Name': 'tag:Enviroment', 'Values': ['Dev']}])
 running_instances = ec2_client.describe_instance_status()
-#for instances in dev_instances:
-    #if instances in running_instances and dev_instances:
-        #id=
-        #ec2_client.start_instances(InstanceIds=[id])
+for instances in dev_instances:
+    if instances in running_instances and dev_instances:
+        id=dev_instances['Tags'][0]['ResourceId']
+        ec2_client.start_instances(InstanceIds=[id])
 #$response = client.start_instances(InstanceIds=[id],)
-#print(response)
-print(dev_instances)
+#print(dev_instances)
+#print(dev_instances.get['ResourceId'])
+#dev = dev_instances['Tags'][0]['ResourceId']
+#print(dev)
