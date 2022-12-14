23b4843541aa2a0f9fe8091b316ca5f339283af3
Josh <josh@mail.com>
2022-12-14 04:52:55 +0000

    final mod to stop instance portion

--- Luit_Py_Projects/week14project2.py
+++ Luit_Py_Projects/week14project2.py
@@ -11,20 +11,23 @@ running_instances = all_running_instances['InstanceStatuses']
 running_instance_id = []
 for i in running_instances:
     running_instance_id.append(i['InstanceId'])
-#print(running_instance_id)
+print(running_instance_id)
 
 dev_instance_tags = dev_instances['Tags']
 dev_instance_Id = []
 for i in dev_instance_tags:
     dev_instance_Id.append(i['ResourceId'])
-#print(dev_instance_Id)
+print(dev_instance_Id)
 
-for _ in dev_instance_Id:
-    if _ in running_instance_id and dev_instance_Id:
-        for ids in range(len(dev_instance_Id)):
-            id=dev_instance_Id[ids]
-            ec2_client.stop_instances(InstanceIds=[id])
-            
+
+for i in dev_instance_Id:
+    if i in running_instance_id:
+        running_dev_Id = i
+        id=running_dev_Id
+        ec2_client.stop_instances(InstanceIds=[id])
+        print(running_dev_Id)
+        
+
 
 
 #for _ in dev_instances:
