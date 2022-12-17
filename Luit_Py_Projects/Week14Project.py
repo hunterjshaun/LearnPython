a31cf444cd0eb3834bab87a93797cce68f7a7ed4
Joshua Hunter <87283224+hunterjshaun@users.noreply.github.com>
2022-12-16 19:55:03 -0500

    Delete Week14Project.py

--- Luit_Py_Projects/Week14Project.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-
-region = 'us-east-1'
-ec2_client = boto3.client('ec2',region_name=region)
-ec2 = boto3.resource('ec2', region_name=region)
-
-running_instance = ec2.instances.filter(Filters=[{'Name': 'instance-state-name','Values': ['running']}])
-print(running_instance.id)
