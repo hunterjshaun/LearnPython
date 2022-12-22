e741e134e7c44091bf61a52dcb6e9dfb915925d0
Josh <josh@mail.com>
2022-12-22 22:49:19 +0000

    modified sqs_send_

--- Luit_Py_Projects/sqs_send_date&time_lambda.py
+++ Luit_Py_Projects/sqs_send_date&time_lambda.py
@@ -1,5 +1,6 @@
 import boto3
 import datetime
+import json
 
 sqs = boto3.client('sqs')
 queue_url = 'https://sqs.us-east-1.amazonaws.com/637919856173/my-queue'
@@ -13,4 +14,8 @@ def lambda_handler(event, context):
         QueueUrl=queue_url,
         MessageBody=current_time
     )
-    print(response)
+    # Return the current date and time in the message body when invoked
+    return { 
+        'statusCode': 200,
+        'body': json.dumps(current_time)
+    }
