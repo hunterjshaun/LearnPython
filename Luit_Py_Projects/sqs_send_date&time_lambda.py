f047486d4042d8355c4b862aca83c71e6fb91547
Joshua Hunter <87283224+hunterjshaun@users.noreply.github.com>
2023-01-01 15:24:02 -0500

    Update sqs_send_date&time_lambda.py

--- Luit_Py_Projects/sqs_send_date&time_lambda.py
+++ Luit_Py_Projects/sqs_send_date&time_lambda.py
@@ -1,21 +1,19 @@
-import boto3
-import datetime
-import json
+import boto3  # import the boto3 library to use the Amazon Web Services (AWS) Simple Queue Service (SQS)
+import datetime # import the datetime module for getting the current time
+import json # import the json library to convert the current time to a json object
 
-sqs = boto3.client('sqs')
-queue_url = 'https://sqs.us-east-1.amazonaws.com/637919856173/my-queue'
+sqs = boto3.client('sqs') # create a client object to interact with the SQS service
+queue_url = 'https://sqs.us-east-1.amazonaws.com/637919856173/my-queue' # URL of the SQS queue
 
 def lambda_handler(event, context):
-    # Get the current time
-    current_time = str(datetime.datetime.now())
+    current_time = str(datetime.datetime.now()) # Get the current time
 
-    # Send the message to the SQS queue
-    response = sqs.send_message(
+    response = sqs.send_message( # Send the message to the SQS queue
         QueueUrl=queue_url,
         MessageBody=current_time
     )
-    # Return the current date and time in the message body when invoked
-    return { 
+
+    return { # Return the current date and time in the message body when invoked
         'statusCode': 200,
         'body': json.dumps(current_time)
     }
